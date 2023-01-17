a4fc8d3330b6ab58c2046c07851659b8028ee11a
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-01-17 19:12:26 +0530

    init

---
+++ src/java_programming_language/loops_flow_statements/Break_stmt/Continue_stmt.java
@@ -0,0 +1,11 @@
+package java_programming_language.loops_flow_statements.Break_stmt;
+
+public class Continue_stmt {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i==5)
+                continue;
+            System.out.println(i);
+        }
+    }
+}
