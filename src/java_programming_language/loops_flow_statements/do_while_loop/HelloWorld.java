1994172c7a88e044900839bc4732902090a3d083
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-01-17 19:11:37 +0530

    init

---
+++ src/java_programming_language/loops_flow_statements/do_while_loop/HelloWorld.java
@@ -0,0 +1,12 @@
+package java_programming_language.loops_flow_statements.do_while_loop;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int counter = 1;
+        do {
+            System.out.println("Hello World");
+            counter++;
+        } while (counter <= 10);
+    }
+
+}
