2874af8e1cafbe8a0baf1fefdfec6c1f143c7273
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-01-18 15:57:48 +0530

    init

---
+++ src/java_programming_language/Patterns/Half_pyramid.java
@@ -0,0 +1,12 @@
+package java_programming_language.Patterns;
+
+public class Half_pyramid {
+    public static void main(String[] args) {
+        for (int line = 1; line <= 4; line++) {
+            for (int star = 1; star <= line; star++) {
+                System.out.print(star);
+            }
+            System.out.println();
+        }
+    }
+}
