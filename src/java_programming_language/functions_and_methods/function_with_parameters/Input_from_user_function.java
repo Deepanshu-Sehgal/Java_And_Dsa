a2d64e30c4b19a564d2097bab98c6c98bf7dd67a
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-01-20 13:55:55 +0530

    init

--- src/java_programming_language/functions_and_methods/function_with_parameters/Input_from_user_function.java
+++ src/java_programming_language/functions_and_methods/function_with_parameters/Input_from_user_function.java
@@ -3,11 +3,16 @@ package java_programming_language.functions_and_methods.function_with_parameters
 import java.util.Scanner;
 
 public class Input_from_user_function {
+    public static int calSum(int a, int b) {
+        return (a + b);
+    }
+
     public static void main(String[] args) {
+        System.out.println("Enter number");
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int sum = a+b;
-        System.out.println(sum);
+        int sum = calSum(a, b);
+        System.out.println("Sum is : " + sum);
     }
 }
