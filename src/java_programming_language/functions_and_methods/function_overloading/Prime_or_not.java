ea9c5ee9ecc788e2fd36289254ed124cf2339388
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-01-21 12:23:11 +0530

    init

---
+++ src/java_programming_language/functions_and_methods/function_overloading/Prime_or_not.java
@@ -0,0 +1,7 @@
+package java_programming_language.functions_and_methods.function_overloading;
+
+public class Prime_or_not {
+    public static void main(String[] args) {
+
+    }
+}
--- src/java_programming_language/functions_and_methods/function_overloading/Sum.java
+++ src/java_programming_language/functions_and_methods/function_overloading/Sum.java
@@ -1,14 +1,18 @@
 package java_programming_language.functions_and_methods.function_overloading;
 
 public class Sum {
-    public static int sum(int a,int b){
-        return a+b;
+
+    //function to cal sum of 2 numbers
+    public static int sum(int a, int b) {
+        return a + b;
     }
-    public static int sum(int a ,int b,int c){
-        return a+b+c;
+
+    //function to cal sum of 3 numbers
+    public static int sum(int a, int b, int c) {
+        return a + b + c;
     }
 
     public static void main(String[] args) {
-        System.out.println(sum(5,6,10));
+        System.out.println(sum(5, 6, 10));
     }
 }
