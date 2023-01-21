fbaaa136667251f4e2e7059220fc9f996e4bf4ad
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-01-21 13:23:27 +0530

    init

--- src/java_programming_language/functions_and_methods/function_overloading/Prime_in_range.java
+++ src/java_programming_language/functions_and_methods/function_overloading/Prime_in_range.java
@@ -1,22 +1,16 @@
 package java_programming_language.functions_and_methods.function_overloading;
 
-public class Prime_or_not {
-
-    //only for n>2
-   /* public static boolean isPrime(int n) {
-        //Corner cases
-        if (n == 2) {
-            return true;
-        }
-        for (int i = 2; i <= n - 1; i++) {
-            if (n % i == 0) { // Completely dividing
-                return false;
+public class Prime_in_range {
+    public static void primeInRange(int n) {
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
             }
         }
-        return true;
-    }*/
+        System.out.println();
 
-    //Optimized solution
+    }
+
     public static boolean isPrime(int n) {
         if (n == 2) {
             return true;
@@ -30,7 +24,6 @@ public class Prime_or_not {
     }
 
     public static void main(String[] args) {
-        System.out.println(isPrime(7));
-
+        primeInRange(100);
     }
 }
