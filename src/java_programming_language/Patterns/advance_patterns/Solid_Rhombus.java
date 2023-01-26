452fb81dc8d02a035452c89f08ce6617cb94bee7
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-01-26 20:19:32 +0530

    init

---
+++ src/java_programming_language/Patterns/advance_patterns/Solid_Rhombus.java
@@ -0,0 +1,19 @@
+package java_programming_language.Patterns.advance_patterns;
+
+public class Solid_Rhombus {
+    public static void Rhombus(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        Rhombus(5);
+    }
+}
