da33503556d85634b583d75e63dc8949b5d1cd03
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-01-26 13:13:27 +0530

    init

---
+++ src/java_programming_language/Patterns/advance_patterns/Inverted_Half_Pyramid.java
@@ -0,0 +1,16 @@
+package java_programming_language.Patterns.advance_patterns;
+
+public class Inverted_Half_Pyramid {
+    public static void half_Pyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        half_Pyramid(5);
+    }
+}
