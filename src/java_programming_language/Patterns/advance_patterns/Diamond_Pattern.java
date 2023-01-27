dbf1c045c2f2ef53934329a8eed034af58c2402a
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-01-27 10:56:07 +0530

    init

--- src/java_programming_language/Patterns/advance_patterns/Diamond_Pattern.java
+++ src/java_programming_language/Patterns/advance_patterns/Diamond_Pattern.java
@@ -6,7 +6,7 @@ public class Diamond_Pattern {
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= 2*i-1; j++) {
+            for (int j = 1; j <= (2*i)-1; j++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -15,7 +15,7 @@ public class Diamond_Pattern {
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= 2*i-1; j++) {
+            for (int j = 1; j <= (2*i)-1; j++) {
                 System.out.print("*");
             }
             System.out.println();
