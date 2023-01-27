48583b9409c967e82a15061804b3d910fe5dedb2
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-01-27 16:16:29 +0530

    init

---
+++ src/java_programming_language/Arrays/Array_as_Function_Argument.java
@@ -0,0 +1,15 @@
+package java_programming_language.Arrays;
+
+public class Array_as_Function_Argument {
+    public static void update(int[] marks) {
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] += 1;
+            System.out.println(marks[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] marks = {34, 56, 23};
+        update(marks);
+    }
+}
