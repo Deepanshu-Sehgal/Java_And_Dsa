c64ecd87089d13974e02498542074b70b66b71ac
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-01-27 15:48:31 +0530

    init

--- src/java_programming_language/Arrays/Input_Output.java
+++ src/java_programming_language/Arrays/Input_Output.java
@@ -1,4 +1,33 @@
 package java_programming_language.Arrays;
 
+import java.util.Scanner;
+
 public class Input_Output {
+    public static void main(String[] args) {
+        //create array
+        int marks[] = new int[100];
+        Scanner sc = new Scanner(System.in);
+
+        //array input
+       /* marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+
+        //Array output
+        System.out.println("phy : " + marks[0]);
+        System.out.println("chem : " + marks[1]);
+        System.out.println("maths : " + marks[2]);
+
+        //update array
+        marks[2] = 100;
+        System.out.println("maths : " + marks[2]);
+
+        int percentage = (marks[0] + marks[1] + marks[2]) / 3;
+        System.out.println("Percentage : " + percentage + "%");*/
+
+        //length of an array
+        System.out.println("length of an array" + marks.length);
+
+
+    }
 }
