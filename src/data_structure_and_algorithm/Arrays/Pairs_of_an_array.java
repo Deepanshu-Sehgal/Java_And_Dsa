cf571abbfd7b80ec27f44cd871b2ed5ed83d24f9
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-01-28 16:12:37 +0530

    init

--- src/data_structure_and_algorithm/Arrays/Pairs_of_an_array.java
+++ src/data_structure_and_algorithm/Arrays/Pairs_of_an_array.java
@@ -4,8 +4,9 @@ public class Pairs_of_an_array {
     public static void pairs(int[] num){
         for (int i = 0; i < num.length; i++) {
             for (int j = i+1; j < num.length; j++) {
-                System.out.print("("+num[i]+","+num[j]+") ");
+                System.out.print("("+num[i]+","+num[j]+")");
             }
+            System.out.println();
         }
     }
     public static void main(String[] args) {
