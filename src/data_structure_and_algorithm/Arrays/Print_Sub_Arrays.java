9589718e6c50a430d7697083504922ea8d55c5b1
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-01-28 17:06:43 +0530

    init

--- src/data_structure_and_algorithm/Arrays/Print_Sub_Arrays.java
+++ src/data_structure_and_algorithm/Arrays/Print_Sub_Arrays.java
@@ -2,15 +2,18 @@ package data_structure_and_algorithm.Arrays;
 
 public class Print_Sub_Arrays {
     public static void subArray(int[] number) {
+        int ts = 0;
         for (int start = 0; start < number.length; start++) { //for finding Start index
             for (int end = start; end < number.length; end++) { //for finding end index
                 for (int k = start; k <= end; k++) { //for printing sub Array's
                     System.out.print(number[k] + " ");
                 }
+                ts++; //counting our total sub array's
                 System.out.println();
             }
             System.out.println();
         }
+        System.out.println("Total sub arrays : " + ts);
     }
 
     public static void main(String[] args) {
