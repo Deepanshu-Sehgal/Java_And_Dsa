1f9f4935636e7b076c85e2b7759d4a2d2132f646
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-01-28 11:09:38 +0530

    init

---
+++ src/data_structure_and_algorithm/Arrays/Searching/Smallest_Number.java
@@ -0,0 +1,18 @@
+package data_structure_and_algorithm.Arrays.Searching;
+
+public class Smallest_Number {
+    public static int get_smallest(int[] number) {
+        int smallest = Integer.MAX_VALUE; //+infinity
+        for (int i = 0; i < number.length; i++) {
+            if (smallest > number[i]) {
+                smallest = number[i];
+            }
+        }
+        return smallest;
+    }
+
+    public static void main(String[] args) {
+        int[] number = {1, 2, 3, 34, 5, 10,};
+        System.out.println("Smallest number is : " + get_smallest(number));
+    }
+}
