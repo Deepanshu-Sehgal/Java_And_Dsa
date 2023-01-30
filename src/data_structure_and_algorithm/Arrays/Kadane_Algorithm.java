7ca95d45e5e63f5c0f30399ae355190b16bb91bb
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-01-30 14:22:55 +0530

    init

---
+++ src/data_structure_and_algorithm/Arrays/Kadane_Algorithm.java
@@ -0,0 +1,23 @@
+package data_structure_and_algorithm.Arrays;
+/*
+Time complexity = O(n)
+*/
+public class Kadane_Algorithm {
+    public static void MaxKadaneSubArray(int[] number) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < number.length; i++) {
+            currSum += number[i];
+            if (currSum < 0) {
+                currSum = 0;
+            }
+            maxSum = Math.max(currSum, maxSum);
+        }
+        System.out.println("Max sub array is = " + maxSum);
+    }
+
+    public static void main(String[] args) {
+        int[] number = {-2, -3, 4, -1, -2, 1, 5, -3};
+        MaxKadaneSubArray(number);
+    }
+}
