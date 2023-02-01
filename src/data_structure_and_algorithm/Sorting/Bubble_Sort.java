10973136bd186d9d44d1b6e1a3d47d1c01cef19a
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-01 12:18:49 +0530

    init

--- src/data_structure_and_algorithm/Sorting/Bubble_Sort.java
+++ src/data_structure_and_algorithm/Sorting/Bubble_Sort.java
@@ -1,19 +1,33 @@
 package data_structure_and_algorithm.Sorting;
 
 public class Bubble_Sort {
+
+    //perform the bubble sort
     public static void bubbleSort(int[] arr) {
         boolean swapped;
+
+        //loop to access each array element
         for (int turn = 0; turn < arr.length - 1; turn++) {
+
+            //check if swapping occurs
             swapped = false;
+
+            //loop to compare adjacent elements
             for (int j = 0; j < arr.length - 1 - turn; j++) {
+
+                //compare two array elements
+                //change > to <  to sort in descending order
                 if (arr[j] > arr[j + 1]) {
-                    //swap
+                    //swap occurs if elements are not in the intended order
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                     swapped = true;
                 }
             }
+
+            // no swapping means the array is already sorted
+            // so no need for further comparison
             if (!swapped)
                 break;
         }
