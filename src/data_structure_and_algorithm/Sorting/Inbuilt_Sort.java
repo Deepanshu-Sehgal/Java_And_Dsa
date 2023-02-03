d04ab997d8999dfea2ebac1b816ced7ec3df1666
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-03 12:35:26 +0530

    init

--- src/data_structure_and_algorithm/Sorting/Inbuilt_Sort.java
+++ src/data_structure_and_algorithm/Sorting/Inbuilt_Sort.java
@@ -1,6 +1,12 @@
 package data_structure_and_algorithm.Sorting;
 
 import java.util.Arrays;
+import java.util.Collections;
+
+/*
+Time complexity = O(n logn)
+*/
+
 
 public class Inbuilt_Sort {
     public static void printArr(String msg, int[] arr) {
@@ -12,7 +18,12 @@ public class Inbuilt_Sort {
     }
     public static void main(String[] args) {
         int[] arr = {5,4,1,3,2};
-        Arrays.sort(arr);
+       Arrays.sort(arr); // inbuilt sort
+
+        Arrays.sort(arr,2,5); // inbuilt sorting by adding start and end indexing or from and to
+
+        //Arrays.sort(arr, Collections.reverseOrder()); // for decending order use collection.reverseOrder()
+        //Arrays.sort(arr ,2,5, Collections.reverseOrder()); // inbuilt sorting by adding start and end indexing or from and to
         printArr("Inbuilt sort",arr);
 
     }
