f6ed4f2c78afb90dd1a2c36cc02ffbe849d8ff4c
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-06 21:40:42 +0530

    init

---
+++ src/data_structure_and_algorithm/two_d_Arrays/Diagonal_sum.java
@@ -0,0 +1,16 @@
+package data_structure_and_algorithm.two_d_Arrays;
+/*
+Samsung microsoft and amazon
+*/
+public class Diagonal_sum {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16}
+        };
+
+    }
+
+}
