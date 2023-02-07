5b5bd8c171d46ff903a8538e70a11f522fdbde7d
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-07 18:04:28 +0530

    init

---
+++ src/java_programming_language/Strings/Strings.java
@@ -0,0 +1,11 @@
+package java_programming_language.Strings;
+
+public class Strings {
+    public static void main(String[] args) {
+        char arr[] = {'a','b','c','d'};
+        String str = "abcd";
+        String str2 = new String("abcd");
+
+        //Strings are IMMUTABLE
+    }
+}
