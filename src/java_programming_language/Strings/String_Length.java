0f4e9cf7e6b88efa4074029eff49293260ec66ed
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-07 18:21:28 +0530

    init

---
+++ src/java_programming_language/Strings/String_Length.java
@@ -0,0 +1,11 @@
+package java_programming_language.Strings;
+
+public class String_Length {
+    public static void main(String[] args) {
+        String fullName = "Deepanshu sehgal";
+        System.out.println("There are " + fullName.length()
+                + " characters in " + fullName);
+
+        //length is function of string class to calculate length of a string
+    }
+}
