7fadaa4973186cbc221ace99b392dddd9fff4272
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-09 12:27:02 +0530

    init

--- src/java_programming_language/Strings/Palindrome_String.java
+++ src/java_programming_language/Strings/Palindrome_String.java
@@ -1,10 +1,18 @@
 package java_programming_language.Strings;
 
 public class Palindrome_String {
+    public static boolean isPalendrome(String str) {
+        for (int i = 0; i <= str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         String str = "racecar";
-        for (int i = 0; i <= str.length() / 2; i++) {
+        System.out.println(isPalendrome(str));
 
-        }
     }
 }
