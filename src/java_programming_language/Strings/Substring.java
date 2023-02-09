bb2d33c71bfd68aa1278bd73b0541b4d352faf47
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-09 12:44:39 +0530

    init

---
+++ src/java_programming_language/Strings/Substring.java
@@ -0,0 +1,8 @@
+package java_programming_language.Strings;
+
+public class Substring {
+    public static void main(String[] args) {
+        String s1= "Deepanshu";
+        System.out.println(s1.substring(2,6)); // ans = epan
+    }
+}
