9bcb790dbbf9a36ffff3507a5ecd4c6ed4478e42
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-09 12:40:23 +0530

    init

---
+++ src/java_programming_language/Strings/Compare.java
@@ -0,0 +1,22 @@
+package java_programming_language.Strings;
+
+public class Compare {
+    public static void main(String[] args) {
+        String s1 = "Deep";
+        String s2 = "Deep";
+        String s3 = new String("Deep");
+
+        if (s1 == s2)
+            System.out.println("String is same");
+        else System.out.println("String is not same");
+
+        if (s1 == s3)
+            System.out.println("String is same");
+        else System.out.println("String is not same");
+
+        //for comparing new string class value with simple string value
+        if (s1.equals(s3))
+            System.out.println("String is same");
+        else System.out.println("String is not same");
+    }
+}
