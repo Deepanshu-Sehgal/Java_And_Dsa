31ffec741cd25dec06543b5b3d101d3b8df9bf00
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-10 17:55:22 +0530

    init

---
+++ src/data_structure_and_algorithm/Bit_Manupulation/Binary_Right_Shift.java
@@ -0,0 +1,7 @@
+package data_structure_and_algorithm.Bit_Manupulation;
+
+public class Binary_Right_Shift {
+    public static void main(String[] args) {
+        System.out.println(6>>1);
+    }
+}
