c85e62818026e0b74a4f2dfa067df73ba2ff4a5c
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-10 16:14:14 +0530

    init

---
+++ src/data_structure_and_algorithm/Bit_Manupulation/Binary_And.java
@@ -0,0 +1,8 @@
+package data_structure_and_algorithm.Bit_Manupulation;
+
+public class Binary_And {
+    public static void main(String[] args) {
+        //Binary AND(&) operator A = 101 & B=110 -> 100 -> (4)
+        System.out.println(5 & 6);
+    }
+}
