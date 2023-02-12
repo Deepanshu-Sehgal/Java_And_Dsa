7ff7a88970feb9e2ec5e6f9b1e63978c738d611f
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-12 15:44:46 +0530

    init

--- src/data_structure_and_algorithm/Bit_Manupulation/Get_ith_Bit.java
+++ src/data_structure_and_algorithm/Bit_Manupulation/Get_ith_Bit.java
@@ -1,7 +1,16 @@
 package data_structure_and_algorithm.Bit_Manupulation;
 
 public class Get_ith_Bit {
-    public static void main(String[] args) {
+    public static int getIthBit(int n, int i) {
+        int bitMask = 1 << i; //shifting 1 by ith position and rest of all bits are 0
+        if ((n & bitMask) == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
 
+    public static void main(String[] args) {
+        System.out.println(getIthBit(10, 2));
     }
 }
