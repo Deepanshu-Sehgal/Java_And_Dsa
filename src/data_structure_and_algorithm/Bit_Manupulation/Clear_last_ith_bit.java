46d532caf9a5229d69e357050f514f57a4069d50
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-14 11:52:28 +0530

    init

---
+++ src/data_structure_and_algorithm/Bit_Manupulation/Clear_last_ith_bit.java
@@ -0,0 +1,12 @@
+package data_structure_and_algorithm.Bit_Manupulation;
+
+public class Clear_last_ith_bit {
+    public static int clearLastIthBit(int n,int i){
+        int bitMask = (~0)<<i;
+        return n & bitMask;
+    }
+    public static void main(String[] args) {
+        System.out.println(clearLastIthBit(15,2));
+
+    }
+}
