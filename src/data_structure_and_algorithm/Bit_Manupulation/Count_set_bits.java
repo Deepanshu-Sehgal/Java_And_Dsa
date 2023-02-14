7c77c7d26e2ac94825f7540517273ef76186c88c
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-14 15:09:52 +0530

    init

---
+++ src/data_structure_and_algorithm/Bit_Manupulation/Count_set_bits.java
@@ -0,0 +1,18 @@
+package data_structure_and_algorithm.Bit_Manupulation;
+
+public class Count_set_bits {
+    public static int countSetBits(int n) {
+        int count = 0;
+        while (n > 0) {
+            if ((n & 1) != 0) { //our lsb
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countSetBits(10));
+    }
+}
