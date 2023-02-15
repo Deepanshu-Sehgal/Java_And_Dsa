62522a5c19ab24a0b0dd8c7d1dc331d1546fe835
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-15 15:45:08 +0530

    init

--- src/Object_oriented_programming/Constructors.java
+++ src/Object_oriented_programming/Constructors.java
@@ -3,6 +3,7 @@ package Object_oriented_programming;
 public class Constructors {
     public static void main(String[] args) {
         Student1 s1 = new Student1();
+        Student1 s2 = new Student1("Deepanshu");
 
     }
 }
@@ -11,7 +12,14 @@ class Student1 {
     String name;
     int rollNo;
 
+    //non parameterized constructor
     Student1() {
         System.out.println("Constructor is called...");
+    }
+
+
+    //parameterized conductor
+    Student1(String name){
+        this.name = name;
     }
 }
