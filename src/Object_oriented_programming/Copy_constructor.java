473c57d950068f2ec0891cdabd539ef14a784766
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-17 15:51:36 +0530

    init

--- src/Object_oriented_programming/Copy_constructor.java
+++ src/Object_oriented_programming/Copy_constructor.java
@@ -5,10 +5,16 @@ public class Copy_constructor {
         Student2 s1 = new Student2();
         s1.name = "Deep";
         s1.rollNo = 123;
-        s1.password = "SDe546";
+        s1.marks[0] = 100;
+        s1.marks[1] = 90;
+        s1.marks[2] = 80;
 
-        Student2 s2 = new Student2(s1);
-        s2.password = "Xyz";
+        Student2 s2 = new Student2(s1);//copy
+        s1.marks[2] = 50;
+        for (int i = 0; i < 3; i++) {
+            System.out.println(s2.marks[i]);
+        }
+
 
     }
 }
@@ -16,16 +22,29 @@ public class Copy_constructor {
 class Student2 {
     String name;
     int rollNo;
-    String password;
+    int marks[];
 
-    //copy constructor
+  /*  //Shallow copy constructor - copy by reference only and changes reflect
     Student2(Student2 s2) {
         this.name = s2.name;
         this.rollNo = s2.rollNo;
+        this.marks = s2.marks;
+    }*/
+
+    //Deep copy constructor - copy creating new array and changes don't reflect
+    Student2(Student2 s2) {
+        marks = new int[3];
+        this.name = s2.name;
+        this.rollNo = s2.rollNo;
+        for (int i = 0; i < marks.length; i++) {
+            this.marks[i] = s2.marks[i];
+        }
     }
+
 
     //non parameterized constructor
     Student2() {
+        marks = new int[3];
         System.out.println("Constructor is called...");
     }
 
