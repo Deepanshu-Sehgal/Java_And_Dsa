d97941160d0c8a5cc559e27de900f51cdf06569f
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-17 16:42:18 +0530

    init

---
+++ src/Object_oriented_programming/Inheritance/Multi_level_inheritance.java
@@ -0,0 +1,30 @@
+package Object_oriented_programming.Inheritance;
+
+public class Multi_level_inheritance {
+    public static void main(String[] args) {
+        Dogs dobby = new Dogs();
+        dobby.eat();
+        dobby.breathe();
+        dobby.legs = 4;
+        dobby.breed = "Pomarian";
+    }
+}
+class Animals {
+    String color;
+
+    void eat() {
+        System.out.println("eat");
+    }
+
+    void breathe() {
+        System.out.println("Breathes");
+    }
+}
+
+class Mammal extends Animal{
+    int legs;
+}
+
+class Dogs extends Mammal{
+    String breed;
+}
