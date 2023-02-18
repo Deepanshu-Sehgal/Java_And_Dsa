87ba821dc32f13b6ce0f9a19b51607bb152aac6d
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-18 15:34:54 +0530

    init

--- src/Object_oriented_programming/Polymorphism/Method_overriding.java
+++ src/Object_oriented_programming/Polymorphism/Method_overriding.java
@@ -3,6 +3,8 @@ package Object_oriented_programming.Polymorphism;
 public class Method_overriding {
 
     public static void main(String[] args) {
+        Dear dear = new Dear();
+        dear.eat();
 
     }
 }
