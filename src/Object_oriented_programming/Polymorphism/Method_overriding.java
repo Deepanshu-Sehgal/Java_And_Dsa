452b0e369b1f47f2228b09eefb216a11a128ad03
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-18 11:38:02 +0530

    init

---
+++ src/Object_oriented_programming/Polymorphism/Method_overriding.java
@@ -0,0 +1,21 @@
+package Object_oriented_programming.Polymorphism;
+
+public class Method_overriding {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Janwar{
+    void eat(){
+        System.out.println("Eats anything");
+    }
+}
+
+class Dear extends Janwar{
+    @Override
+    void eat() {
+        System.out.println("Eats grass");
+    }
+}
