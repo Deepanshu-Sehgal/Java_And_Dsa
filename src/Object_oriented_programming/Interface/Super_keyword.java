9d5b1354602ddc2080bbe22e717f842323518c2c
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-20 15:50:26 +0530

    init

--- src/Object_oriented_programming/Interface/Super_keyword.java
+++ src/Object_oriented_programming/Interface/Super_keyword.java
@@ -2,7 +2,10 @@ package Object_oriented_programming.Interface;
 
 public class Super_keyword {
     public static void main(String[] args) {
-        Horse h = new Horse();
+        Animal5 h = new Horse();
+        h.print();
+        Animal5 h2 = new Animal5();
+        h2.print();
     }
 }
 
@@ -10,11 +13,18 @@ class Animal5{
     Animal5(){
         System.out.println("animal constructor is called");
     }
+
+    void print(){
+        System.out.println("Animal");
+    }
 }
 
 class Horse extends Animal5{
     Horse(){
         super();
         System.out.println("Horse constructor is called");
+    }
+    void print(){
+        System.out.println("Horse");
     }
 }
