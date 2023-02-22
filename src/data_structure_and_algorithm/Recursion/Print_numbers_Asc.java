ae218ac55bd017bd0900e07c5aeaa7888ac47c2f
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-22 13:47:13 +0530

    init

--- src/data_structure_and_algorithm/Recursion/Print_numbers_Asc.java
+++ src/data_structure_and_algorithm/Recursion/Print_numbers_Asc.java
@@ -8,7 +8,7 @@ public class Print_numbers_Asc {
     public static void printIncreasingOrder(int n) {
 
         if (n == 1) { //base case
-            System.out.print(1);
+            System.out.print(n + " ");
             return;
         }
         printIncreasingOrder(n - 1);
