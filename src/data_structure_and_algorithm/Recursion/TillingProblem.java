d0913ae306aa11b9dc6e9780cdf1647a5611a722
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-24 20:07:22 +0530

    init

---
+++ src/data_structure_and_algorithm/Recursion/TillingProblem.java
@@ -0,0 +1,11 @@
+package data_structure_and_algorithm.Recursion;
+
+public class TillingProblem {
+    public static void main(String[] args) {
+
+    }
+
+    public static int TotalTiles(int n) {
+        return 3;
+    }
+}
