f5a3d787afbb2776cf2ca3b96eb79b8d6db5fb0c
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-24 17:08:55 +0530

    init

--- src/data_structure_and_algorithm/Recursion/Print_x_power_n_Oprimized.java
+++ src/data_structure_and_algorithm/Recursion/Print_x_power_n_Oprimized.java
@@ -1,22 +1,25 @@
 package data_structure_and_algorithm.Recursion;
 
 //time complexity = O(Log n)
+
 public class Print_x_power_n_Oprimized {
     public static void main(String[] args) {
-        System.out.println(OptimizedPower(2, 10));
+        System.out.println(OptimizedPower(2, 5));
     }
 
     public static int OptimizedPower(int number, int power) {
-        if (power == 0) {
+        if (power == 0) { //base case
             return 1;
         }
-        int halfPower = OptimizedPower(number, power / 2) * OptimizedPower(number, power / 2);
+
+        int halfPower = OptimizedPower(number, power / 2);
+        int halfPowerSq =  halfPower * halfPower;
 
         //power is odd
         if (power % 2 != 0) {
-            halfPower = number * halfPower;
+            halfPowerSq = number * halfPowerSq;
         }
 
-        return halfPower;
+        return halfPowerSq;
     }
 }
