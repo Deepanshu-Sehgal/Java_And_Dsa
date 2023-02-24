6ec5dbd32340fa792aa610a29c3870c34dcd3696
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-24 13:23:52 +0530

    init

---
+++ src/data_structure_and_algorithm/Recursion/First_Occurence.java
@@ -0,0 +1,19 @@
+package data_structure_and_algorithm.Recursion;
+
+public class First_Occurence {
+    public static void main(String[] args) {
+        int[] arr = {8, 3, 6, 9, 5, 10, 22, 5, 3};
+        System.out.println(firstOccurence(arr, 5, 0));
+    }
+
+    public static int firstOccurence(int[] arr, int key, int i) {
+        if (i == arr.length) //base case
+            return -1;
+
+
+        if (arr[i] == key) {
+            return i;
+        }
+        return firstOccurence(arr, key, i + 1);
+    }
+}
