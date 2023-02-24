a18b833d9466372fbd5c841e09c32b40cfad391d
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-24 14:12:17 +0530

    init

--- src/data_structure_and_algorithm/Recursion/First_Occurence.java
+++ src/data_structure_and_algorithm/Recursion/First_Occurence.java
@@ -1,5 +1,7 @@
 package data_structure_and_algorithm.Recursion;
 
+//time complexity and space complexity = O(n)
+
 public class First_Occurence {
     public static void main(String[] args) {
         int[] arr = {8, 3, 6, 9, 5, 10, 22, 5, 3};
---
+++ src/data_structure_and_algorithm/Recursion/Last_Occurence.java
@@ -0,0 +1,21 @@
+package data_structure_and_algorithm.Recursion;
+
+public class Last_Occurence {
+    public static void main(String[] args) {
+            int[] arr = {8, 3, 6, 9, 5, 10, 22, 5, 3};
+            System.out.println(LastOccurence(arr, 5, 0));
+        }
+
+    public static int LastOccurence ( int[] arr, int key, int i){
+        if (i == arr.length)
+            return -1;
+
+        int isFound = LastOccurence(arr, key, i + 1);
+
+        if (isFound == -1 && arr[i] == key) {
+            return i;
+        }
+
+        return isFound;
+    }
+}
