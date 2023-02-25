0b80d683ca8d67d532c87de178c01b4dddbcd5e8
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-25 11:02:31 +0530

    init

--- src/data_structure_and_algorithm/Recursion/TillingProblem.java
+++ src/data_structure_and_algorithm/Recursion/TillingProblem.java
@@ -1,11 +1,28 @@
 package data_structure_and_algorithm.Recursion;
 
+/*
+company amazon
+*/
 public class TillingProblem {
     public static void main(String[] args) {
 
+        System.out.println(totalTiles(5));
+
     }
 
-    public static int TotalTiles(int n) {
-        return 3;
+    public static int totalTiles(int n) { // 2 x n (floor size)
+
+        if (n==0 || n==1)
+            return 1;
+
+        //kaam
+        //vertical choice
+        int verticalTiles = totalTiles(n-1);  //function n - 1
+
+        //horizontal choice
+        int horizontalTiles = totalTiles(n-2); //function n - 2
+
+        return verticalTiles + horizontalTiles;
+
     }
 }
