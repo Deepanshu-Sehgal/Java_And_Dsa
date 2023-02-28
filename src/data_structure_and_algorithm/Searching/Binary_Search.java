163e41a41ff0c0d03637b6929b87e249333482cb
Deepanshu-Sehgal <deepanshusehgal.112@gmail.com>
2023-02-28 13:55:14 +0530

    init

--- src/data_structure_and_algorithm/Searching/Binary_Search.java
+++ src/data_structure_and_algorithm/Searching/Binary_Search.java
@@ -4,13 +4,13 @@ public class Binary_Search {
     public static int binary_Search(int[] numbers, int key) {
         int start = 0, end = numbers.length - 1;
         while (start <= end) {
-            int mid = (start + end) / 2; //mid
+            int mid = start + (end - start) / 2; //mid
             if (numbers[mid] == key) {
                 return mid;
             }
-            if (numbers[mid] < key) { // if mid less then key update right
+            if (numbers[mid] < key) { // if mid less than key update right
                 start = mid + 1;
-            } else { //else mid more then key update left
+            } else { //else mid more than key update left
                 end = mid - 1;
             }
         }
@@ -20,7 +20,7 @@ public class Binary_Search {
     public static void main(String[] args) {
         int[] number = {2, 3, 4, 5, 6, 67, 89, 90}; //sorted array
         int key = 6; //key to search
-        int result=binary_Search(number, key);
+        int result = binary_Search(number, key);
         System.out.println(result);
     }
 }
