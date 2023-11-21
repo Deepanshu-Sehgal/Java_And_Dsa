8b2a1041733aab989ed5a2da38acbb7d46044757
Deepanshu-sehgal <deepanshusehgal.112@gmail.com>
2023-11-21 09:22:34 +0530

    init

--- src/java_programming_language/Patterns/practice/titli.java
+++ src/java_programming_language/Patterns/practice/titli.java
@@ -6,10 +6,7 @@ public class titli {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
-            for (int j = 1; j < 4 - i + 1; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j < 4 - i; j++) {
+            for (int j = 0; j < 2 * (4 - i); j++) {
                 System.out.print(" ");
             }
             for (int j = 1; j <= i; j++) {
@@ -21,10 +18,7 @@ public class titli {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
-            for (int j = 1; j < 4 - i + 1; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j < 4 - i; j++) {
+            for (int j = 0; j < 2 * (4 - i); j++) {
                 System.out.print(" ");
             }
             for (int j = 1; j <= i; j++) {
