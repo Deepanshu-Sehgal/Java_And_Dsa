923c22589f380897522e609388cccc592dd0cb99
Deepanshu-sehgal <deepanshusehgal.112@gmail.com>
2023-12-16 22:27:20 +0530

    init

---
+++ src/data_structure_and_algorithm/two_d_Arrays/practice/PrintNumbersOfSeven.java
@@ -0,0 +1,15 @@
+package data_structure_and_algorithm.two_d_Arrays.practice;
+
+public class PrintNumbersOfSeven {
+    public static void main(String[] args) {
+        int[][] arr = {{4,5,7},{5,7,6}};
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] == 7)
+                    count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
---
+++ src/data_structure_and_algorithm/two_d_Arrays/practice/SumOfSecondRow.java
@@ -0,0 +1,14 @@
+package data_structure_and_algorithm.two_d_Arrays.practice;
+
+public class SumOfSecondRow {
+    public static void main(String[] args) {
+        int[][] array = {{4, 5, 6}, {11, 4, 3}, {5, 6, 2}};
+        int sum = 0;
+
+            for (int j = 0; j < array[0].length; j++) {
+                    sum = sum + array[1][j];
+            }
+        System.out.println(sum);
+    }
+}
+
