799ef477b0561e3b4d27c70fa905ee5a2ac4a92e
Deepanshu-sehgal <deepanshusehgal.112@gmail.com>
2024-09-05 14:19:19 +0530

    init

---
+++ src/data_structure_and_algorithm/Arrays/SecondLargestElement.java
@@ -0,0 +1,24 @@
+package data_structure_and_algorithm.Arrays;
+
+public class SecondLargestElement {
+    public static void main(String[] args) {
+        int arr[] = {10,7,8,15,34};
+
+        System.out.println(secondLargest(arr));
+
+    }
+
+    static int secondLargest(int arr[]) {
+        int result = -1, largest = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[largest]) {
+                result = largest;
+                largest = i;
+            } else if (arr[i] != arr[largest]) {
+                if (result == -1 || arr[i] > arr[result])
+                    result = i;
+            }
+        }
+        return result;
+    }
+}
