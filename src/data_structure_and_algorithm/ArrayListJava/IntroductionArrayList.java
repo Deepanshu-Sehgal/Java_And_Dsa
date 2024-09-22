2d0200e34649e3ebd578bbfb6fb07c5a6fef3e20
Deepanshu-sehgal <deepanshusehgal.112@gmail.com>
2024-09-22 21:29:49 +0530

    init

---
+++ src/data_structure_and_algorithm/ArrayListJava/IntroductionArrayList.java
@@ -0,0 +1,12 @@
+package data_structure_and_algorithm.ArrayListJava;
+import java.util.*;
+
+public class IntroductionArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(30);
+
+        System.out.println(list);
+
+    }
+}
