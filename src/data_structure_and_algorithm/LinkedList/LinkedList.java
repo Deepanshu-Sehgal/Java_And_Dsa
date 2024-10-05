914f934e2ae6f038526cf7192004af62baee241a
Deepanshu Sehgal <deepanshusehgal.112@gmail.com>
2024-10-05 23:04:03 +0530

    init

--- src/data_structure_and_algorithm/LinkedList/LinkedList.java
+++ src/data_structure_and_algorithm/LinkedList/LinkedList.java
@@ -46,13 +46,29 @@ public class LinkedList {
         tail = newNode;
     }
 
+    public void print() {
+        if (head == null) {
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
 //        ll.head = new Node(1);
 //        ll.head.next = new Node(2);
+        ll.print();
         ll.addFirst(2);
         ll.addFirst(1);
+        ll.print();
         ll.addLast(3);
         ll.addLast(4);
+        ll.print();
     }
 }
