cf30d6db15c326f68c4aa67cb73f786e4efab719
Deepanshu Sehgal <deepanshusehgal.112@gmail.com>
2024-10-06 13:52:39 +0530

    init

--- src/data_structure_and_algorithm/LinkedList/LinkedList.java
+++ src/data_structure_and_algorithm/LinkedList/LinkedList.java
@@ -13,11 +13,13 @@ public class LinkedList {
 
     public static Node head;
     public static Node tail;
+    public static int size;
 
     //Meathods
     public void addFirst(int data) {
         //step 1 - create new node
         Node newNode = new Node(data);
+        size++;
         if (head == null) {
             head = tail = newNode;
             return;
@@ -34,6 +36,7 @@ public class LinkedList {
     public void addLast(int data) {
         //step 1 - create new node
         Node newNode = new Node(data);
+        size++;
         if (head == null) {
             head = tail = newNode;
             return;
@@ -59,6 +62,26 @@ public class LinkedList {
         System.out.println("null");
     }
 
+    public void add(int index, int data) {
+        if (index == 0) {
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i = 0;
+        while (i < index - 1) {
+            temp = temp.next;
+            i++;
+        }
+
+        //i = idx-1;temp -> prev
+        newNode.next = temp.next;
+        temp.next = newNode;
+
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
 //        ll.head = new Node(1);
@@ -70,5 +93,8 @@ public class LinkedList {
         ll.addLast(3);
         ll.addLast(4);
         ll.print();
+        ll.add(2, 9);
+        ll.print();
+        System.out.println(ll.size);
     }
 }
