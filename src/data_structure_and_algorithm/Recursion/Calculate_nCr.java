0d8a60177af555a3392518b5d404023ee9b1951b
Deepanshu Sehgal <deepanshusehgal.112@gmail.com>
2025-01-09 22:30:11 +0530

    init

---
+++ src/data_structure_and_algorithm/Recursion/Calculate_nCr.java
@@ -0,0 +1,13 @@
+package data_structure_and_algorithm.Recursion;
+
+public class Calculate_nCr {
+    public static void main(String[] args) {
+
+        System.out.println(nCr(5,3));
+    }
+
+    static int nCr(int n, int r) {
+        if (n == r || r == 0) return 1;
+        return nCr(n - 1, r - 1) + nCr(n - 1, r);
+    }
+}
