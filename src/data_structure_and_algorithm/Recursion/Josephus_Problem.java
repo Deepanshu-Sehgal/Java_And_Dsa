2e32f01bae0c58472d01e92c6b165b592276cff5
Deepanshu Sehgal <deepanshusehgal.112@gmail.com>
2025-01-10 00:03:56 +0530

    init

--- src/data_structure_and_algorithm/Recursion/Josephus_Problem.java
+++ src/data_structure_and_algorithm/Recursion/Josephus_Problem.java
@@ -1,4 +1,14 @@
 package data_structure_and_algorithm.Recursion;
 
 public class Josephus_Problem {
+
+    //Goli marne vli game hai ye ki 5 log hai and hr 3re bande ko gun se marna hai
+    public static void main(String[] args) {
+        System.out.println(josephus(5,3));
+    }
+
+    static int josephus(int n, int k) {
+        if (n == 1) return 0;
+        return (josephus(n - 1, k) + k) % n;
+    }
 }
