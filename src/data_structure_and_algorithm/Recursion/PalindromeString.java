9bbd1d64255f97538604307cd7685762724de436
Deepanshu Sehgal <deepanshusehgal.112@gmail.com>
2025-01-10 13:01:17 +0530

    init

---
+++ src/data_structure_and_algorithm/Recursion/PalindromeString.java
@@ -0,0 +1,21 @@
+package data_structure_and_algorithm.Recursion;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("racecar"));
+
+    }
+
+    static boolean isPalindrome(String s) {
+        return isPalindromeHelper(s, 0, s.length() - 1);
+    }
+
+    static boolean isPalindromeHelper(String s, int left, int right) {
+        if (left >= right) return true;
+        if (s.charAt(left) == s.charAt(right)) {
+            return isPalindromeHelper(s, left + 1, right - 1);
+        }
+        return false;
+
+    }
+}
