e878430b751c15a51787bb51da0fa4e2d877e907
Deepanshu Sehgal <deepanshusehgal.112@gmail.com>
2025-01-11 19:18:09 +0530

    init

--- src/data_structure_and_algorithm/Recursion/Print_x_power_n_Optimized.java
+++ src/data_structure_and_algorithm/Recursion/Print_x_power_n_Optimized.java
@@ -2,7 +2,7 @@ package data_structure_and_algorithm.Recursion;
 
 //time complexity = O(Log n)
 
-public class Print_x_power_n_Oprimized {
+public class Print_x_power_n_Optimized {
     public static void main(String[] args) {
         System.out.println(OptimizedPower(2, 5));
     }
