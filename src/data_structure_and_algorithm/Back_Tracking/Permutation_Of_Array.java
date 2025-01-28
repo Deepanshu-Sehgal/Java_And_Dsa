ccd354c3d0a9852f9f1b20c96f93aeab195e4ceb
Deepanshu Sehgal <deepanshusehgal.112@gmail.com>
2025-01-28 10:43:11 +0530

    init

--- src/data_structure_and_algorithm/Back_Tracking/Permutation_Of_Array.java
+++ src/data_structure_and_algorithm/Back_Tracking/Permutation_Of_Array.java
@@ -1,19 +1,33 @@
 package data_structure_and_algorithm.Back_Tracking;
 
+import java.util.ArrayList;
+
 public class Permutation_Of_Array {
     public static void main(String[] args) {
         int a[] = {1, 2, 3};
-        permute(a, 0, a.length - 1);
+        ArrayList<int[]> ans = new ArrayList<>();
+        permute(a, 0, a.length - 1,ans);
+        System.out.println("Print array");
+
+        for (int[] e : ans) {
+            printArrPerm(e);
+        }
+
     }
 
-    static void permute(int a[], int l, int r) {
+    static void permute(int a[], int l, int r,ArrayList<int[]> ans) {
         if (l == r) {
             printArrPerm(a);
+            int[] temp = new int[a.length];
+            for (int i = 0; i < a.length; i++) {
+                temp[i] = a[i];
+            }
+            ans.add(temp);
             return;
         }
         for (int i = l; i <= r; i++) {
             swap(a, i, l);
-            permute(a, l + 1, r);
+            permute(a, l + 1, r,ans);
             swap(a, i, l); //back tracking
         }
     }
