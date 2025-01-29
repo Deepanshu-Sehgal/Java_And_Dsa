0646e4bdeed4763e61be0ca12dc53da65960c544
Deepanshu Sehgal <deepanshusehgal.112@gmail.com>
2025-01-29 19:18:09 +0530

    init

---
+++ src/data_structure_and_algorithm/Back_Tracking/SmartKeypadProblem.java
@@ -0,0 +1,38 @@
+package data_structure_and_algorithm.Back_Tracking;
+
+import java.util.ArrayList;
+
+public class SmartKeypadProblem {
+    public static void main(String[] args) {
+        String input = "53";
+        System.out.println(smartKeypad(input));
+
+
+    }
+
+    static ArrayList<String> smartKeypad(String input) {
+        String map[] = {
+                "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+        };
+        ArrayList<String> ans = new ArrayList<>();
+        smartKeypadHandler(input,map,0,ans,"");
+        return ans;
+
+    }
+
+    static void smartKeypadHandler(String input, String map[], int index, ArrayList<String> ans, String cur) {
+        if (index == input.length()) {
+            ans.add(cur);
+            return;
+        }
+
+        int keypadNumber = input.charAt(index) - '0';
+        String keypadString = map[keypadNumber];
+
+        for (int i = 0; i < keypadString.length(); i++) {
+            cur = cur + keypadString.charAt(i);
+            smartKeypadHandler(input, map, index+1, ans, cur);
+            cur = cur.substring(0, cur.length() - 1);
+        }
+    }
+}
