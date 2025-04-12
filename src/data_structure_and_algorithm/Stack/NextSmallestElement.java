f8284c34676ca393b083c9ab3fe80cbfa2c2b915
Deepanshu Sehgal <deepanshusehgal.112@gmail.com>
2025-04-12 17:45:13 +0530

    init

--- src/data_structure_and_algorithm/Stack/NextSmallestElement.java
+++ src/data_structure_and_algorithm/Stack/NextSmallestElement.java
@@ -1,15 +1,35 @@
 package data_structure_and_algorithm.Stack;
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
 
 public class NextSmallestElement {
     public static void main(String[] args) {
-        int a[] = {1, 6, 4, 10, 2, 4};
-        int ans[] = smallerOnRight(a);
+        int a[] = {1, 4, 3, 2, 7, 9, 6, 4};
+        int ans[] = manMinArray(a);
         for (int e : ans) {
             System.out.print(e + " ");
         }
 
+    }
+
+    static int[] manMinArray(int a[]) {
+        int n = a.length;
+        int left[] = smallerOnLeft(a);
+        int right[] = smallerOnRight(a);
+
+        int ans[] = new int[n];
+
+        Arrays.fill(ans, Integer.MIN_VALUE);
+        for (int i = 0; i < n; i++) {
+            int wl = right[i] - left[i] - 1;
+            ans[wl - 1] = Math.max(ans[wl - 1], a[i]);
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            ans[i] = Math.max(ans[i], ans[i + 1]);
+        }
+        return ans;
     }
 
     static int[] smallerOnLeft(int a[]) {
