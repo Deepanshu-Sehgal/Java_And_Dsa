b57c2a1a1f7d5ae452d65990eda7030c37ec1fa8
Deepanshu Sehgal <deepanshusehgal.112@gmail.com>
2025-04-12 16:48:46 +0530

    init

--- src/data_structure_and_algorithm/Stack/NextSmallestElement.java
+++ src/data_structure_and_algorithm/Stack/NextSmallestElement.java
@@ -5,7 +5,7 @@ import java.util.ArrayDeque;
 public class NextSmallestElement {
     public static void main(String[] args) {
         int a[] = {1,6,4,10,2,4};
-        int ans[] = smallerOnLeft(a);
+        int ans[] = smallerOnRight(a);
         for (int e:ans){
             System.out.print(e+" ");
         }
@@ -23,6 +23,28 @@ public class NextSmallestElement {
             }
             if (stack.isEmpty()) {
                 ans[i] = -1;
+            } else {
+                ans[i] = stack.peek();
+            }
+
+            stack.push(i);
+
+        }
+        return ans;
+
+    }
+
+    static int[] smallerOnRight(int a[]) {
+        int n = a.length;
+        int ans[] = new int[n];
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        for (int i = n-1; i >= 0; i--) {
+            while (!stack.isEmpty() && a[stack.peek()] >= a[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                ans[i] = n;
             } else {
                 ans[i] = stack.peek();
             }
