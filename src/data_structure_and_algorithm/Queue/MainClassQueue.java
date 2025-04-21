b45427ad2420470fbfd7575fc6b53e74e4632e77
Deepanshu Sehgal <deepanshusehgal.112@gmail.com>
2025-04-21 18:52:06 +0530

    init

--- src/data_structure_and_algorithm/Queue/MainClassQueue.java
+++ src/data_structure_and_algorithm/Queue/MainClassQueue.java
@@ -2,20 +2,31 @@ package data_structure_and_algorithm.Queue;
 
 public class MainClassQueue {
     public static void main(String[] args) {
-        MyCircularQueue queue = new MyCircularQueue(5);
-        queue.enqueue(10);
-        queue.enqueue(20);
-        queue.enqueue(30);
-        queue.enqueue(40);
-        queue.enqueue(50);
-        queue.printQueue();
-        System.out.println(queue.peek());
-        queue.printQueue();
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
-        queue.enqueue(80);
-        queue.enqueue(90);
-        queue.printQueue();
+//        MyCircularQueue queue = new MyCircularQueue(5);
+//        queue.enqueue(10);
+//        queue.enqueue(20);
+//        queue.enqueue(30);
+//        queue.enqueue(40);
+//        queue.enqueue(50);
+//        queue.printQueue();
+//        System.out.println(queue.peek());
+//        queue.printQueue();
+//        System.out.println(queue.dequeue());
+//        System.out.println(queue.dequeue());
+//        queue.enqueue(80);
+//        queue.enqueue(90);
+//        queue.printQueue();
+
+        QueueUsingTwoStacks qTwoStacks = new QueueUsingTwoStacks();
+        qTwoStacks.enqueue(10);
+        qTwoStacks.enqueue(20);
+        qTwoStacks.enqueue(30);
+        qTwoStacks.enqueue(40);
+        System.out.println(qTwoStacks.dequeue());
+        System.out.println(qTwoStacks.dequeue());
+        System.out.println(qTwoStacks.dequeue());
+        System.out.println(qTwoStacks.dequeue());
+        System.out.println(qTwoStacks.dequeue());
 
     }
 }
--- src/data_structure_and_algorithm/Queue/QueueUsingTwoStacks.java
+++ src/data_structure_and_algorithm/Queue/QueueUsingTwoStacks.java
@@ -1,4 +1,24 @@
 package data_structure_and_algorithm.Queue;
 
+import java.util.ArrayDeque;
+
 public class QueueUsingTwoStacks {
+    private ArrayDeque<Integer> stack1 = new ArrayDeque<>();
+    private ArrayDeque<Integer> stack2 = new ArrayDeque<>();
+
+    public void enqueue(int data) {
+        stack1.push(data);
+    }
+
+    public int dequeue
+            () {
+        if (stack1.isEmpty() && stack2.isEmpty()) return -1;
+
+        if (!stack2.isEmpty()) return stack2.pop();
+
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+        return stack2.pop();
+    }
 }
